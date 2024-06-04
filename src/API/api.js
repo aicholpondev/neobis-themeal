aa640ac7e221033f0194d84ebe801501ec6260f2
aicholpondev <aicholponsultanii@gmail.com>
2024-06-04 19:38:49 +0600

    create all components,get api key

---
+++ src/API/api.js
@@ -0,0 +1,3 @@
+
+export const API ="www.themealdb.com/api/json/v1/1/random.php";
+export const API_SEARCH ="www.themealdb.com/api/json/v1/1/ search.php?s=Arrabiata"
---
+++ src/components/MealCard/MealCard.jsx
@@ -0,0 +1,35 @@
+import{API,API_SEARCH} from "../../API/api"
+import { useEffect ,useState} from "react";
+import axios from "axios";
+
+export default function MealCard () {
+    const [searchMeal, setSearchMeal] = useState([]);
+    const [getMeal, setGetMeal] = useState([]);
+    const [inpValue, setInpValue] = useState("");
+
+    useEffect (() =>{
+        axios (API).then((data) => setGetMeal(data.data.meals[0]));
+    },[]);
+    function handleSearchOnChange(e) {
+        setInpValue(e.target.value);
+        handleSearch(e);
+    }
+      
+      const handleSearch = async(e) =>{
+        e.preventDefault();
+        try{
+            let response = await axios
+            .get(API_SEARCH +{inpValue})
+            .then((data) =>{
+                setSearchMeal(data.data.meals);
+          console.log(searchMeal);
+
+            })
+        }catch(error){
+            console.error("Error fetching meal:", error);
+
+        }
+      }
+
+      
+    }
